52a178aed62e82e7ec0711659ffdae2cd56533a4
mtryoutlander <32582120+mtryoutlander@users.noreply.github.com>
2023-04-14 15:41:55 -0500

    remove build error in the LvManager

--- Assets/Scripts/LvManager.cs
+++ Assets/Scripts/LvManager.cs
@@ -1,23 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LvManager : MonoBehaviour
 {
     public void LoadScene(string sceneName)
     {
-        //load the scene
-        Application.LoadLevel(sceneName);
+        SceneManager.LoadScene(sceneName);
     }
     public void QuitGame()
     {
-        if (Application.isEditor)
-        {
-            UnityEditor.EditorApplication.isPlaying = false;
-        }
-        else
-        {
-            Application.Quit();
-        }
+        Application.Quit();
     }
 }
